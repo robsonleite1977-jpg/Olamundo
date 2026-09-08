50bf385f4ac64f258e39488bcebf5a100bc15daa
robsonleite1977-jpg <robsonleite1977@gmail.com>
2026-09-08 18:38:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-# pip install flask
+import os
+from flask import Flask, render_template
 
-# → importa a ferramenta Flask para usar neste arquivo.
-from flask import Flask
-
-# → cria a "aplicação" (seu site).
 app = Flask(__name__)
 
-# → diz ao Flask: "quando alguém acessar a página principal (/), faça o que está abaixo".
 @app.route("/")
+def index():
+    return render_template("index.html")
 
-# → define a função que será executada. O texto entre aspas é o que o navegador vai mostrar.
-def ola_mundo():
-    return "<h1>Olá, Mundo!</h1><p>Esta é minha primeira página em Python.</p>"
-
-#  → diz: "se este arquivo for rodado diretamente, inicie o servidor".
 if __name__ == "__main__":
-
-    # → liga o site e o debug=True mostra erros detalhados enquanto você desenvolve.
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
